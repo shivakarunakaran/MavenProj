c6271a1efd337693443e7a52c3efcec40f969e0e
shivakarunakaran <92569072+shivakarunakaran@users.noreply.github.com>
2023-07-31 12:42:04 +0530

    Update Demobasecls.java

--- src/test/java/basepkg/Demobasecls.java
+++ src/test/java/basepkg/Demobasecls.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 
-public class Iterabase
+public class Iterabasecls
 {
 	
 	public WebDriver driver;
