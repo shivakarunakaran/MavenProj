4f9a3ae3c17953523c51e26f875ef73a053f099d
Shiva Karunakaran <letter2karan@gmail.com>
2023-10-04 14:21:10 +0530

    isla

--- src/test/java/page/Ebaypage.java
+++ src/test/java/page/Ebaypage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.internal.invokers.AbstractParallelWorker.Arguments;
+//import org.testng.internal.invokers.AbstractParallelWorker.Arguments;
 
 public class Ebaypage 
 {
