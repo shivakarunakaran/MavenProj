2f446e9ec9cfeab806ac2e9f8b3a27bf8f93502a
Shiva Karunakaran <letter2karan@gmail.com>
2024-01-16 11:24:27 +0530

    isla

--- src/test/java/test/Ebaytest.java
+++ src/test/java/test/Ebaytest.java
@@ -9,9 +9,11 @@ public class Ebaytest extends Ebaybasecls
 	@Test
 	public void Ebay()
 	{			
+		
 		Ebaypage ebay=new Ebaypage(driver);
 		ebay.loginoptions("islarose@gmail.com");
 		ebay.batsearch("cricket bat");
 		ebay.mobilevalues("iphone");
+		
 	}
 }
